1459021ef347a737968df44ca9f3bf3f39a5d526
Isaddora Freitas <isaddorafreitasar@gmail.com>
2023-06-22 14:39:38 -0300

    feat: add process to test load balancer

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,6 +8,11 @@ export interface DateResponse {
 @Injectable()
 export class AppService {
   date(): DateResponse {
+    // Processo demorado para testar o load balancer
+    for (let i = 0; i < 1000000000; i++) {
+      i + 1;
+    }
+
     const date = new Date();
 
     const dateResponse = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
