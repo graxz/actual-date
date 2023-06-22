84d9e55acc1a11f4e5064bbe6a4a7dd1fbf9af29
Isaddora Freitas <isaddorafreitasar@gmail.com>
2023-06-22 12:34:10 -0300

    feat: add date module

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
 
+export interface DateResponse {
+  date: string;
+  time: string;
+}
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  date(): DateResponse {
+    const date = new Date();
+
+    const dateResponse = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
+    const timeResponse = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+
+    return {
+      date: dateResponse,
+      time: timeResponse,
+    };
   }
 }
